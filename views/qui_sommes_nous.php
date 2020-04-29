4e1d1fbde382a67c66510a028b074de834b3a962
unknown <cthelot@ensc.fr>
2020-04-29 15:32:15 +0200

    qui sommes nous ?

---
+++ views/qui_sommes_nous.php
@@ -0,0 +1,22 @@
+<html>
+    <?php include  "../includes/head.html"; ?>
+    <?php include "../includes/navbar.html"; ?>
+
+    <body>
+      <h1> Qui sommes-nous ? </h1>
+      <div class="texte_accueil">
+          <p class="description_accueil">
+          Nous sommes Aurélie, Clara, Judith et Oriane quatres étudiantes en première année à l'ENSC (Ecole Nationale
+          Supérieure de Cognitique).
+          <br><br>
+          (Mais qu'est ce que l'ENSC ? <a href="https://ensc.bordeaux-inp.fr/fr"><i> Cliquez ici ! </i></a>)
+          <br><br>
+          Nous avons réalisé ce site dans le cadre du projet de modélisation des connaissances de notre cours de Gestion
+          de compétence.
+          Vous trouverez dans l'onglet "Fabriquer ces propres bouteilles de lavande" les modélisations MASK-I correspondant
+          à notre travail !
+          </p>
+      </div>
+      <div class="petite_image"> <img src="../images/ENSC.png" alt="Image de l'ENSC"> </div>
+    </body>
+  </html>
