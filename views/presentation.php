9ae91bdabcb48ee68491234f96583dbca0d8ff92
unknown <cthelot@ensc.fr>
2020-04-29 12:26:55 +0200

    29/04

---
+++ views/presentation.php
@@ -0,0 +1,29 @@
+<html>
+    <?php include  "../includes/head.html"; ?>
+    <?php include "../includes/navbar.html"; ?>
+
+    <body>
+      <h1> Une tradition provençale </h1>
+      <div class="texte_accueil">
+          <p class="description_accueil">
+            La fabrication de bouteilles de lavande (ou fuseau de lavande) est une tradition provençale qui date du 18ème siècle.
+            Les paysans souhaitant pouvoir conserver le délicat parfum de la fleur pendant longtemps, ils imaginèrent une manière de
+            la tisser à l'aide d'un ruban de lin, tissu commun et peu cher à l'époque. Il est aujourd'hui remplacé par un ruban de satin.
+            Les fleurs ainsi tressées laissent échapper leur parfum pendant plusieurs mois.
+            On prit alors l'habitude de mettre ces fuseaux dans les armoires à vêtement, autant pour parfumer le ligne que pour éloigner
+            les mites.
+            <br><br>
+            Cette tradition était profondémment ancrée dans les familles provençales. Ainsi la mère tissait un fuseau pour le donner à
+            sa fille au moment de son mariage.
+            <br><br>
+            Malheureusement, étant donné que la fabrication d'un fuseau peut prendre beaucoup de temps, celui-ci fut remplacé dans Les
+            armoires à vêtements par les sachets de lavande.
+            <br><br>
+          </p>
+          </div>
+        <div class="petite_image"> <img src="../images/Lavande_bouteilles_6.png" alt="Images lavandes 6"> </div>
+      </body>
+      <!-- <script src="../slick-master/slick/jquery.min.js"></script> -->
+      <script src="../slick-master/slick/slick.js"></script>
+      <script src="../scripts/carousel.js"></script>
+</html>
