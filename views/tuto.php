23e39ddb04a5c03566a2f9efec5205f52980c462
relourocket <aurelie.gass@gmail.com>
2020-05-01 15:39:03 +0100

    Page tuto texte

---
+++ views/tuto.php
@@ -0,0 +1,43 @@
+<html>
+<?php include  "../includes/head.html"; ?>
+<?php include "../includes/navbar.html"; ?>
+
+
+<body>
+
+    <h1>Réaliser ses propres bouteilles de lavandes</h1>
+
+    <p class="description_accueil">
+        <ul>
+            <li>Prendre 50 brins de lavande où n divisible par 2 et pas par 4(sélectionnés pour leur qualité)</li>
+            <li>Éliminer les ramifications des brins de lavandes afin de n’obtenir que des tiges lisses terminées par un
+                épi floral</li>
+            <li>Attacher les brins ensembles le plus près possible des fleurs comme sur la photo ci-dessous</li>
+            <img src="../images/image_1.jpg" class="moyenne_image">
+            <li>Plier les tiges et les répartir autour de la lavande pour englober le bouquet comme sur la photo
+                suivante</li>
+            <img src="../images/image_2.jpg" class="moyenne_image">
+            <li>Prendre un ruban de couleur d’environ</li>
+            <li> Tresser le ruban dans les brins de la façon suivante : soulever les brins 2 par 2 et passez le ruban
+                une fois au-dessus des 2 brins, et une fois en-dessous, en alternant et en serrant bien à chaque passage
+                (faire 3 tours de la bouteille de cette manière)</li>
+            <li>Au bout de 3 tours, continuer de tresser les brins comme sur l'image ci-dessous</li>
+            <img src="../images/image_3.jpg" class="moyenne_image">
+            <li>Une fois le sommet de la bouteille atteint (environ 10 cm avant le bout des brins), arrêter de tresser
+                et enrouler le ruban autour de tous les brins de lavandes sur la moitié de la hauteur du sommet
+                (5 cm environ) comme illustré ci-dessous </li>
+            <img src="../images/image_4.jpg" class="moyenne_image">
+            <li>Enrouler l’autre moitié de la hauteur du sommet avec un morceau de papier brillant (papier aluminium)
+                pour faire le bouchon</li>
+            <li>Enrouler le ruban de couleur sur quelques tours de plus pour maintenir le morceau de papier
+                aluminium comme le montre la photo suivante</li>
+            <img src="../images/image_5.jpg" class="moyenne_image">
+            <li>Couper le reste de ruban à l’aide de ciseaux</li>
+            <li>Décorer ensuite la bouteille à votre convenance (avec quelques fleurs en ruban par exemple)
+            comme sur l'exemple donné ci-dessous</li>
+            <img src="../images/image_6.jpg" class="moyenne_image">
+        </ul>
+    </p>
+
+
+</body>
