382d8acd8455ce2b7f644c69065386c55aaef216
relourocket <aurelie.gass@gmail.com>
2020-05-04 14:52:32 +0100

    Page des représentations MASK

---
+++ views/mask.php
@@ -0,0 +1,27 @@
+<html>
+<?php include  "../includes/head.html"; ?>
+<?php include "../includes/navbar.html"; ?>
+
+<body>
+
+    <h1>La représentation MASK-I</h1>
+
+    <br>
+
+    <h5 class="description_accueil">Ici vous trouverez les représentations en MASK-I pour pouvoir reproduire les
+    bouteilles de lavandes chez vous.</h5>
+
+    <br><br>
+
+    <h4>Diagramme des concepts (<a href="../files/Concept.xmind" download>Télécharger le diagramme des concepts au format Xmind</a>)</h4>
+    <img class="image_diagrammes" src="../images/Concepts.PNG">
+
+    <h4>Diagramme des tâches (<a href="../files/Taches.xmind" download>Télécharger le diagramme des tâches au format Xmind</a>)</h4>
+    <img class="image_diagrammes" src="../images/Taches.PNG">
+
+    <h4>Diagramme des processus (<a href="../files/Taches.xmind" download>Télécharger le diagramme des processus au format Xmind</a>)</h4>
+    <img class="image_diagrammes" src="../images/Processus.PNG">
+
+    
+
+</body>
